bcb895517a23c0eb518daf8049b64fdc397677b5
Alexandru Fulga <alexandrufulga13@gmail.com>
2024-12-07 01:08:01 +0100

    Adjusting DTOs and creating first migration.

--- Program.cs
+++ Program.cs
@@ -1,73 +1,15 @@
 using games_api.Contracts;
+using games_api.Data;
+using games_api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
-const string GetGameEndpointName = "GetGame";
-
-List<GameDto> games = [
-    new (
-        1,
-        "Street Fighter II",
-        "Fighting",
-        19.99M,
-        new DateOnly(1992, 7, 15)
-    ),
-    new (
-        2,
-        "Final Fantasy XIV",
-        "Role-playing",
-        59.99M,
-        new DateOnly(2010, 9, 30)
-    ),
-    new (
-        1,
-        "FIFA 23",
-        "Sports",
-        69.99M,
-        new DateOnly(2022, 9, 27)
-    ),
-];
-
-// GET /games
-app.MapGet("/games", () => games);
-
-// GET /games/{id}
-app.MapGet("/games/{id}", (int id) => games.Find(g => g.Id == id))
-.WithName(GetGameEndpointName);
-
-// POST /games
-
-app.MapPost("/games", (GameDto newGame) =>
-{
-    GameDto game = new(
-        games.Count + 1,
-        newGame.Name,
-        newGame.Genre,
-        newGame.Price,
-        newGame.ReleaseDate
-    );
-
-    games.Add(game);
-
-    return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
-});
-
-// PUT /games/{id}
-app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
-{
-    int index = games.FindIndex(game => game.Id == id);
-
-    games[index] = new GameDto(
-        id,
-        updatedGame.Name,
-        updatedGame.Genre,
-        updatedGame.Price,
-        updatedGame.ReleaseDate
-    );
-
-    return Results.NoContent();
-});
+app.MapGamesEndpoints();
 
 app.MapGet("/", () => "Hello World!");
 
